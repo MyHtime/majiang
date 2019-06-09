212334a9a632cebfd1b484f843af3dcfa2c51c48
PN IN H HP <602827790@qq.com>
2019-06-09 20:22:29 +0800

    extract github config from code to properties

--- src/main/java/cn/tecnpan/majiang/helloworld/controller/AuthorizeController.java
+++ src/main/java/cn/tecnpan/majiang/helloworld/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import cn.tecnpan.majiang.helloworld.dto.AccessToken;
 import cn.tecnpan.majiang.helloworld.dto.GitHubUser;
 import cn.tecnpan.majiang.helloworld.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,23 @@ public class AuthorizeController {
     @Autowired
     private GitHubProvider gitHubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam("code") String code, @RequestParam("state") String state) {
         AccessToken accessToken = new AccessToken();
+        accessToken.setClient_id(clientId);
+        accessToken.setClient_secret(clientSecret);
         accessToken.setCode(code);
-        accessToken.setRedirect_uri("http://localhost:8887/callback");
+        accessToken.setRedirect_uri(redirectUri);
         accessToken.setState(state);
-        accessToken.setClient_id("6ec56ffc2179dd5c6116");
-        accessToken.setClient_secret("68bee3acbd3d4602ad1950e4cc813876e81f87cf");
         String token = gitHubProvider.getAccessToken(accessToken);
         GitHubUser gitHubUser = gitHubProvider.getGitHubUser(token);
         System.out.println(gitHubUser.getName());
